a77ea85ef3574df94680b25088a529dc8636c102
Juan Reyero <juan@juanreyero.com>
2013-05-13 19:50:10 +0200

    Returns meta dictionary

--- orgbib/docmeta.py
+++ orgbib/docmeta.py
@@ -19,31 +19,29 @@ import subprocess
 from getch import getch
 
 
-def print_meta(meta, fields):
+def print_meta(meta, fields, new_meta=None):
+    if new_meta is None:
+        new_meta = {}
+    new_meta = dict(meta.items() + new_meta.items())
     for field in fields:
-        value = meta.get(field, '')
+        value = new_meta.get(field, '')
         if not isinstance(value, basestring) and hasattr(value, '__iter__'):
             value = u' and '.join(value)
+        if not field in meta and value:
+            value = '(%s)' % value
         print "%15s [%s]: %s" % (field, field[0], value)
 
 def interactive_meta(bookfile):
     meta = docid.guess_meta(bookfile)
 
     fields = ['author', 'date', 'isbn', 'language', 'publisher',
-              'title', 'google', 'books at google', 'open library',
+              'title', 'url', 'google', 'books at google', 'open library',
               'wikipedia']
-    index = {}
-    for field in fields:
-        index[field[0]] = field
     formatters = collections.defaultdict(lambda: lambda v: v)
     formatters['author'] = lambda a: u' & '.join(a.split(u' and '))
 
     print '\n', bookfile
     print_meta(meta, fields)
-    meta['google'] = meta.get('title', '')
-    meta['books at google'] = meta.get('title', '')
-    meta['open library'] = meta.get('isbn', '')
-    meta['wikipedia'] = meta.get('isbn', '')
 
     def open_google(search):
         if not search and 'title' in meta:
@@ -52,14 +50,6 @@ def interactive_meta(bookfile):
             webbrowser.open('http://google.com/' +
                             urllib.urlencode({'q': search.encode('utf-8')}))
     formatters['google'] = lambda v: open_google(v)
-
-    def open_google_books(search):
-        if not search and 'title' in meta:
-            search = meta['title']
-        if search:
-            webbrowser.open('http://books.google.com/books?' +
-                            urllib.urlencode({'q': search.encode('utf-8')}))
-    formatters['books at google'] = lambda v: open_google_books(v)
 
     def open_google_books(search):
         if not search and 'title' in meta:
@@ -88,31 +78,44 @@ def interactive_meta(bookfile):
     def cl_option(fname, val):
         if fname == u'author':
             fname = u'authors'
+        elif fname == u'url':
+            return ''
         return u'--' + fname + u'=' + val
 
+    index = {}
+    for field in fields:
+        index[field[0]] = field
+    newmeta = {}
     while True:
         print 'Option (enter to next book, q to exit): ',
         option = getch()
-        if option == 'q':
-            sys.exit(1)
-        if not option.strip():
+        if option == 'q' or not option.strip():
             print
-            return
+            return dict(meta.items() + newmeta.items()), option
+
         field = index.get(option, '')
         if not field:
+            print
             continue
         print field
-        #print u'' % meta.get(field, u'')
-        #value = raw_input(u'Value : ').decode(sys.stdin.encoding)
-        print u'Value (%s): ' % meta.get(field, u''),
+        valuestr = meta.get(field, u'')
+        if valuestr:
+            valuestr = ' (%s)' % valuestr
+        print u'Value%s: ' % valuestr,
         value = raw_input('').decode(sys.stdin.encoding)
-        value = formatters[field](value)
-        if value:
-            devnull = codecs.open(os.devnull, 'w', encoding='utf-8')
-            subprocess.call(['ebook-meta', cl_option(field, value), bookfile],
-                             stdout=devnull, stderr=devnull)
+        formatted_value = formatters[field](value)
+        if formatted_value:
+            option = cl_option(field, value)
+            if option:
+                devnull = codecs.open(os.devnull, 'w', encoding='utf-8')
+                if subprocess.call(['ebook-meta', option, bookfile],
+                                    stdout=devnull, stderr=devnull):
+                    print "** Error calling ebook-meta on", self.txtbook_file,\
+                      "(maybe DRMed book?)"
+            newmeta[field] = value
+
             meta = docid.guess_meta(bookfile)
-            print_meta(meta, fields)
+            print_meta(meta, fields, newmeta)
 
 
 def as_main():
@@ -129,7 +132,10 @@ def as_main():
             sys.exit(1)
 
     for fname in files:
-        interactive_meta(fname)
+        meta, key = interactive_meta(fname)
+        print meta
+        if key == 'q':
+            return
 
 if __name__ == '__main__':
     as_main()
